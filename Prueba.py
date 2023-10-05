5db66f28a82b234171caf2323ad0afe421afba38
Dan <danroal@ciencias.unam.mx>
2023-10-05 06:01:38 -0600

    pruebas

--- Prueba.py
+++ Prueba.py
@@ -13,8 +13,34 @@ def evaluaciones_f():
     
     return lista_evaluar
 
-print(evaluaciones_f())
+
 cadena = "x  "
 variables = [letra.lstrip().rstrip() for letra in cadena.split(',')]
 
-print(variables)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+s = np.linspace(0.3, 1.7, 10)
+t = s
+
+dis = np.random.normal(1, 0.1, 10)
+tiem = np.random.normal(1, 0.1, 10)
+
+fig, ax= plt.subplots()
+fig2, ax2= plt.subplots()
+
+ax.scatter(s,t, s=150)
+
+ax.set(ylabel='Tiempo (s)', xlabel='Distancia (m)',
+       title='Calculo de la velocidad', xlim=[0,2], ylim=[0,2])
+ax.grid()
+
+ax2.scatter(dis, tiem, s=150)
+
+ax2.set(ylabel='Tiempo (s)', xlabel='Distancia (m)',
+       title='Calculo de la velocidad', xlim=[0,2], ylim=[0,2])
+ax2.grid()
+
+plt.show()
